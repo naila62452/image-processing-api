e30082504b1d5de01953de9b040b5b7b1e864161
Naila Nosheen <naila.nosheen.uos@gmail.com>
2021-09-19 22:53:25 +0200

    image processing api
    created the api for resizing the image

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-const myFunc = (num: number): number => {
-    return num * num;
-  };
-  
-  export default myFunc;
+// const myFunc = (num: number): number => {
+//   return num * num;
+// };
+// export default myFunc;
+
+import express from 'express';
+import sharp from 'sharp';
+const app = express();
+const port = 4000;
+
+app.get('/images', (req, res) => {
+  const widthResize = req.query.width;
+  const heightResize = req.query.height;
+  // const imageName: Number = (req.query.filename);
+  let width, height;
+  if (widthResize) {
+    width = Number(widthResize);
+  }
+  if (heightResize) {
+    height = Number(heightResize);
+  }
+  sharp('image-processing-api/images/assets' + req.query.filename)
+    .resize({ width: width, height: height })
+    .toBuffer()
+    .then((data) => {
+      res.end(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to the port http://localhost:${port}`);
+});
